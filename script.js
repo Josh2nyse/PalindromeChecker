c718dcb0fef873c27e3c3906aa84c6f3cc4971f8
Josh2nyse <chukwuebukaibeawuchijoshua@gmail.com>
2025-06-27 23:10:57 -0700

    updated code

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const checkBtn = document.getElementById("check-btn");
 const textInput = document.getElementById("text-input")
 const result = document.getElementById("result");
+const geneBtn = document.getElementById("generate-Btn");
 
 
+const palindromeList = [
+	"A man a plan a canal Panama",
+	"No lemon no melon",
+	"Was it a car or a cat i saw",
+	"Able was i are i saw elba",
+	"Race car",
 
+];
 function isPalindrome (str) {
 	const cleanedStr = str.replace(/[^a-z0-9]/gi, "").toLowerCase();
 	const reversedStr = cleanedStr.split("").reverse().join("");
@@ -23,7 +31,17 @@ checkBtn.addEventListener("click", () => {
 		result.innerText = `${inputValue} is a palindrome`;
 	}
 	else {
-		result.innerText = `${inputValue} is a palindrome`;
+		result.innerText = `${inputValue} is not a palindrome`;
 	}
-
+	return;
 });
+
+geneBtn.addEventListener("click", () => {
+	const randomIndex = Math.floor(Math.random() * palindromeList.length);
+	const randomPalindome = palindromeList[randomIndex];
+	result.innerText = `Suggested Palindrome: "${randomPalindome}"`;
+});
+
+
+
+
